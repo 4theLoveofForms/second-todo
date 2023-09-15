875ace24a482877527afd8f8267f852744826046
4theLoveofForms <tangledrootsinfo@gmail.com>
2023-09-15 17:36:06 +0100

    learning

--- src/App.js
+++ src/App.js
@@ -1,10 +1,70 @@
-
+import React, { useState, useEffect } from 'react'
 import './App.css';
+//importing components
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+
 
 function App() {
+  
+  // state stuff
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]); 
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos ] = useState([]);
+
+  console.log("todos", todos);
+  // use effect
+  useEffect( () => {
+    getLocalTodos();
+  }, [] )
+  useEffect( () => {
+    filterHandler();
+    saveLocalTodos();
+  }, [todos, status] )
+  // functions
+  const filterHandler = () => {
+    switch(status){
+      case 'completed' :
+        setFilteredTodos( todos.filter(todo => todo.completed === true ) )
+        break;
+      case 'uncompleted' :
+        setFilteredTodos( todos.filter( todo => todo.completed === false ) )
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+
+    }
+  }
+  // save to local
+  const saveLocalTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }
+  const getLocalTodos = () => {
+    if(localStorage.getItem('todos') === null) {
+      localStorage.setItem('todos', JSON.stringify([]));
+    }
+    else {
+      let todoLocal = JSON.parse(localStorage.getItem('todos')); 
+      console.log(todoLocal);
+      setTodos(todoLocal);
+    }
+  }
+
   return (
     <div className="App">
-      <h1>Hello react</h1>
+      <header >
+        <h1>George todo list</h1>
+      </header>
+      <Form inputText={inputText} 
+            setInputText={setInputText} 
+            setTodos={setTodos} 
+            todos={todos}
+            setStatus={setStatus}
+            
+            />
+      <TodoList todos={todos} setTodos={setTodos} filteredTodos={filteredTodos} />
     </div>
   );
 }
